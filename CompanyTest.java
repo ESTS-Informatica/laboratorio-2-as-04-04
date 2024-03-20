9b07a85bd0124a81add45ad00cab02953e3f087e
Crisar004 <202200168@estudantes.ips.pt>
2024-03-20 09:19:54 +0000

    Nivel 4 feito

--- CompanyTest.java
+++ CompanyTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,7 +16,33 @@ public class CompanyTest {
         seller1 = new User("Fernando Fernandes", "966777101", "fefe@remax.pt");
         seller2 = new User("Rodrigo Rodrigues", "966777152", "roro@remax.pt");
     }
+    
+    @Test
+    public void testRegisterClient() {
+        assertTrue(company.registerClient(client1));
+        assertEquals(1, company.getClients().size());
+        assertTrue(company.getClients().contains(client1));
+    }
 
+    @Test
+    public void testRegisterClients() {
+        assertTrue(company.registerClient(client1));
+        assertTrue(company.registerClient(client2));
+        assertEquals(2, company.getClients().size());
+        assertTrue(company.getClients().contains(client1));
+        assertTrue(company.getClients().contains(client2));
+    }
+     @Test
+    public void testRegisterClientDuplicate() {
+        company.registerClient(client1);
+        company.registerClient(client1);
+    }
+
+    @Test
+    public void testRegisterClientNull() {
+        company.registerClient(null);
+    }
+    
     @Test
     public void testConstructor() {
         assertNotNull(company.getClients());
